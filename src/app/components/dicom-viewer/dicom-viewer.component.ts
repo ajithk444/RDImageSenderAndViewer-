24107d580bfb27032926baa71ae611588e4b21a5
mperezcorrales <manuel.perezc@pucp.pe>
2018-05-08 02:45:47 -0500

    Se agregaron herramientas al dicom viewer. Coreccion de botones en el case_view

--- src/app/components/dicom-viewer/dicom-viewer.component.ts
+++ src/app/components/dicom-viewer/dicom-viewer.component.ts
@@ -2,8 +2,30 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AngularFireStorage } from 'angularfire2/storage';
 import { Observable } from 'rxjs/Observable';
+import { Location } from '@angular/common';
 
 declare var dwv: any;
+
+
+// gui overrides
+
+// decode query
+dwv.utils.decodeQuery = dwv.utils.base.decodeQuery;
+// progress
+dwv.gui.displayProgress = function () { };
+// window
+dwv.gui.getWindowSize = dwv.gui.base.getWindowSize;
+// get element
+dwv.gui.getElement = dwv.gui.base.getElement;
+// refresh element
+dwv.gui.refreshElement = dwv.gui.base.refreshElement;
+
+// Image decoders (for web workers)
+dwv.image.decoderScripts = {
+  'jpeg2000': 'assets/dwv/decoders/pdfjs/decode-jpeg2000.js',
+  'jpeg-lossless': 'assets/dwv/decoders/rii-mango/decode-jpegloss.js',
+  'jpeg-baseline': 'assets/dwv/decoders/pdfjs/decode-jpegbaseline.js'
+};
 
 @Component({
   selector: 'app-dicom-viewer',
@@ -13,27 +35,38 @@ declare var dwv: any;
 export class DicomViewerComponent implements OnInit {
 
   dicomUrl: string;
+  private dwvApp: any;
 
   constructor(private route: ActivatedRoute,
-    private router: Router) { }
+    private router: Router,
+    private location: Location) { }
 
   ngOnInit() {
     this.dicomUrl = this.route.snapshot.queryParamMap.get('dicomUrl');
-    
-    console.log('dicomUrl', this.dicomUrl);
 
-      // base function to get elements
-      dwv.gui.getElement = dwv.gui.base.getElement;
-      dwv.gui.displayProgress = function (percent) { };
+    // create the dwv app
+    this.dwvApp = new dwv.App();
+    // initialise with the id of the container div
+    this.dwvApp.init({
+      'containerDivId': 'dwv',
+      'fitToWindow': true,
+      'tools': ['Scroll', 'WindowLevel', 'ZoomAndPan'],
+      'shapes': ['Ruler'],
+      'isMobile': false
+    });
+    // load dicom data 
+    this.dwvApp.loadURLs([this.dicomUrl]);
+  }
 
-      // create the dwv app
-      const app = new dwv.App();
-      // initialise with the id of the container div
-      app.init({
-        'containerDivId': 'dwv'
-      });
-      // load dicom data
-      app.loadURLs([this.dicomUrl]);
+  onClick(event) {
+    if (this.dwvApp) {
+      console.log('llego acaaaaa');
+      this.dwvApp.onChangeTool(event);
+    }
+  }
+
+  goBack() {    
+    this.location.back();
   }
 
 }
